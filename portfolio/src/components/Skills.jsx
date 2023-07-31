157979e2240c0b89b08df9bb9788308c4971dbb8
siddeshg907 <121315414+siddeshg907@users.noreply.github.com>
2023-07-31 22:18:09 +0530

    first try

---
+++ portfolio/src/components/Skills.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <section id="skills">
+      <div className="skills-card">
+        <img className="skills-card-img" src="/path/to/skill_image.jpg" alt="Skill" />
+        <p className="skills-card-name">Skill Name</p>
+      </div>
+      {/* Add more skills cards as needed */}
+    </section>
+  );
+};
+
+export default Skills;
