08dc8b0ad44ffb404f72cb198c7a967575fc30b6
siddeshg907 <121315414+siddeshg907@users.noreply.github.com>
2023-08-24 21:24:42 +0530

    copied

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
+import Statistics from "./Components/Statistics";
+import Skills from "./Components/Skills";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hari om</h1>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Statistics />
+      <Contact />
     </div>
   );
 }
